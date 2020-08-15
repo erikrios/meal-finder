552ed1778d89c72d185b051a90b27d7c5a8bc9ca
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-08-15 18:05:48 +0700

    Change into class

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,15 +1,17 @@
-function DataSource(onSuccess, onFailed) {
-    this.onSuccess = onSuccess;
-    this.onFailed = onFailed;
-}
-
-DataSource.prototype.searchMeal = function (keyword) {
-    const filteredMeals = meals.filter(meal =>
-        meal.name.toUpperCase().includes(keyword.toUpperCase()));
-
-    if (filteredMeals.length) {
-        this.onSuccess(filteredMeals);
-    } else {
-        this.onFailed(`${keyword} is not found`);
+class DataSource {
+    constructor(onSuccess, onFailed) {
+        this.onSuccess = onSuccess;
+        this.onFailed = onFailed;
     }
-};
+
+    searchMeal(keyword) {
+        const filteredMeals = meals.filter(meal =>
+            meal.name.toUpperCase().includes(keyword.toUpperCase()));
+
+        if (filteredMeals.length) {
+            this.onSuccess(filteredMeals);
+        } else {
+            this.onFailed(`${keyword} is not found`);
+        }
+    }
+}
